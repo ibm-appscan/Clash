4709161714bb30dfdd8b69ecfa6319830d8be16e
ibm-appscan <31466486+ibm-appscan@users.noreply.github.com>
2020-07-30 15:19:28 +0800

    Update CheckIn.py

--- CheckIn.py
+++ CheckIn.py
@@ -7,7 +7,7 @@ import requests,json,os
 # sckey = os.environ["SCKEY"]
 
 # 填入glados账号对应cookie
-cookie = os.environ["da59bde2fcc0d167abbf144e9efd199341596083188"]
+cookie = os.environ["__cfduid=da59bde2fcc0d167abbf144e9efd199341596083188; _ga=GA1.2.1897939625.1596083194; _gid=GA1.2.1022595244.1596083194; koa:sess=eyJ1c2VySWQiOjQ1NjIxLCJfZXhwaXJlIjoxNjIyMDEzNDM3MDA3LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=ZESXtj5KPiX5rAmZc-3R0dAphEE"]
 
 
 def start():    
