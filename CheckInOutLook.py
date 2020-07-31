b298e6a6a7e640964a0f9e89bd870030f164ddc3
ibm-appscan <31466486+ibm-appscan@users.noreply.github.com>
2020-07-31 10:35:08 +0800

    Update and rename CheckIn.py to CheckInOutLook.py

--- CheckInOutLook.py
+++ CheckInOutLook.py
@@ -2,7 +2,7 @@ import requests
 
 
 # 填入glados账号对应cookie
-cookie = "__cfduid=da59bde2fcc0d167abbf144e9efd199341596083188; _ga=GA1.2.1897939625.1596083194; _gid=GA1.2.1022595244.1596083194; koa:sess=eyJ1c2VySWQiOjQ1NjIxLCJfZXhwaXJlIjoxNjIyMDEzNDM3MDA3LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=ZESXtj5KPiX5rAmZc-3R0dAphEE"
+cookie = "__cfduid=da59bde2fcc0d167abbf144e9efd199341596083188; _ga=GA1.2.1897939625.1596083194; _gid=GA1.2.1022595244.1596083194; koa:sess=eyJ1c2VySWQiOjQ1NjIxLCJfZXhwaXJlIjoxNjIyMDgxNjMyNDYwLCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=S0BbeT6kLj3yPHdG1zHvimaxkOk"
 
 
 def start():
@@ -17,7 +17,7 @@ def start():
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
         print(mess)
-        print('剩余' + time + '天')
+        print('ibm-appscan@outlook.com剩余' + time + '天')
 
 
 def main_handler(event, context):
