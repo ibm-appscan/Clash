2cc65daac0840d428830e688490c1a699f212833
ibm-appscan <31466486+ibm-appscan@users.noreply.github.com>
2020-10-22 17:19:05 +0800

    Update CheckInOutLook.py

--- CheckInOutLook.py
+++ CheckInOutLook.py
@@ -12,13 +12,12 @@ def start():
     referer = 'https://glados.rocks/console/checkin'
     checkin = requests.post(url, headers={'cookie': cookie, 'referer': referer})
     state = requests.get(url2, headers={'cookie': cookie, 'referer': referer})
-    print(state.json())
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
         time = state.json()['data']['leftDays']
         # time = time.split('.')[0]
-        print('OUTLOOK剩余' + time + '天')
+        print('OUTLOOK剩余' + str(time) + '天')
 
 
 def main_handler(event, context):
