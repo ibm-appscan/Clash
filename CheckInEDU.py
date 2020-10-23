e9e3590813729483aee9c44eace0b93d632077cc
ibm-appscan <31466486+ibm-appscan@users.noreply.github.com>
2020-10-23 10:37:47 +0800

    Update CheckInEDU.py

--- CheckInEDU.py
+++ CheckInEDU.py
@@ -5,13 +5,28 @@ import os
 # 填入glados账号对应cookie
 cookie = os.environ["COOKIE"]
 
+header = {
+    'Host': 'glados.rocks',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Content-Type': 'application/json;charset=utf-8',
+    'Content-Length': '26',
+    'Origin': 'https://glados.rocks',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+	'Referer': 'https://glados.rocks/console/checkin',
+	'Cookie': cookie,
+}
+
 
 def start():
     url = "https://glados.rocks/api/user/checkin"
     url2 = "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    checkin = requests.post(url, headers={'cookie': cookie, 'referer': referer})
-    state = requests.get(url2, headers={'cookie': cookie, 'referer': referer})
+    checkin = requests.post(url, headers=header)
+    state = requests.get(url2, headers=header)
 
     if 'message' in checkin.text:
         mess = checkin.json()['message']
